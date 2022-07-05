076679d9e0419d0f4f9fa94d8041e544ed6b685a
dryptomim <32720316+dryptomim@users.noreply.github.com>
2022-07-05 20:53:53 +0200

    fix typo

--- sandbox/adapters/adapters.ts
+++ sandbox/adapters/adapters.ts
@@ -3,6 +3,6 @@ import { Entity } from "../entity.js";
 export interface Adapter {
     // send a String to Matrix, Activitypup, etc
     send(data: String):void;
-    // recives a String and map the data
+    // receives a String and map the data
     receive():Entity;
 }
