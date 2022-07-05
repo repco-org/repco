d02f008fc3d8c62c965d93c02970531cf7c328b4
dryptomim <32720316+dryptomim@users.noreply.github.com>
2022-07-05 20:52:55 +0200

    fix typo

--- sandbox/adapters/adapters.ts
+++ sandbox/adapters/adapters.ts
@@ -4,5 +4,5 @@ export interface Adapter {
     // send a String to Matrix, Activitypup, etc
     send(data: String):void;
     // recives a String and map the data
-    recieve():Entity;
+    receive():Entity;
 }
