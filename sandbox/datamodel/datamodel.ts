3912c2653ba6b8e098324b51e2e248ab920774f3
nemi1012 <nemi1012@h-ka.de>
2022-07-05 12:52:53 +0200

    add interface proposal for the datamodel

---
+++ sandbox/datamodel/datamodel.ts
@@ -0,0 +1,10 @@
+import { Entity, EntityBatch } from "../entity";
+
+export interface Datamodel{
+    //add Header of our Datamodel
+    createRecord(sourceData: Promise<EntityBatch>) : Entity;
+    //validate types etc. msy check if data exist
+    validate(record: Entity) : Boolean;
+    //persist to local database may trigger Adapters/RepcoApi?
+    persist(record: Entity) : void;
+}
