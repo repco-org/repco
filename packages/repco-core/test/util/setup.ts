c165b3b9191e012d58392e29b7b7318a4cf3ca35
Franz Heinzmann (Frando) <frando@unbiskant.org>
2022-08-01 19:22:26 +0200

    cleanup

--- packages/repco-core/test/util/setup.ts
+++ packages/repco-core/test/util/setup.ts
@@ -5,44 +5,21 @@ import Dotenv from 'dotenv'
 const COMPOSE_FILE = '../../test/docker-compose.test.yml'
 const ENV_FILE = '../../test/test.env'
 
-Dotenv.config({
-  path: ENV_FILE
-})
+Dotenv.config({ path: ENV_FILE })
 
 export async function setup (teardown: Test["teardown"]) {
-  await spawn('docker-compose', [
-    '-f', 
-    COMPOSE_FILE, 
-    'up', 
-    '-d', 
-    '--remove-orphans'
-  ], { 
-    stdio: 'inherit' 
-  })
-
+  await spawn('docker-compose', ['-f', COMPOSE_FILE, 'up', '-d', '--remove-orphans'])
   await new Promise(resolve => setTimeout(resolve, 500))
-
-  await spawn('yarn', [
-    'prisma', 
-    'migrate', 
-    'deploy'
-  ], { 
-    stdio: 'inherit', 
+  await spawn('yarn', ['prisma', 'migrate', 'deploy'], { 
     cwd: '../repco-prisma' 
   })
-
   teardown(async () => {
-    await spawn('docker-compose', [
-      '-f',
-      COMPOSE_FILE, 
-      'down'
-    ], { 
-      stdio: 'inherit' 
-    })
+    await spawn('docker-compose', ['-f', COMPOSE_FILE, 'down'])
   }, {})
 }
 
 function spawn(command: string, args: string[], opts: SpawnOptions = {}): Promise<void> & { child: ChildProcess }{
+  if (!opts.stdio) opts.stdio = 'inherit'
   console.error(`>> ${command} ${args.map(x => `"${x}"`).join(' ')}`)
   const child = spawnProcess(command, args, opts)
   const promise = new Promise((resolve, reject) => {
