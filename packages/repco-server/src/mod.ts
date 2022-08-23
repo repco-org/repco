13ec765d65f5068aaeeb2e12332199cf7286ca38
nemi1012 <nemi1012@h-ka.de>
2022-08-23 13:22:08 +0200

    fix

--- packages/repco-server/src/mod.ts
+++ packages/repco-server/src/mod.ts
@@ -1,15 +1,19 @@
-// @ts-ignore
 import 'express-async-errors'
 import * as error from './error.js'
 import cors from 'cors'
 import express from 'express'
+import { makeExecutableSchema } from '@graphql-tools/schema'
+import { graphqlHTTP } from 'express-graphql'
 import { PrismaClient } from 'repco-core'
 import Routes from './routes.js'
 
 export function runServer(prisma: PrismaClient, port: number) {
   const app = express()
+
   app.use(express.json({ limit: '100mb' }))
   app.use(cors())
+  app.use(express.urlencoded({ extended: true }))
+
   app.use((req, res, next) => {
     res.locals.prisma = prisma
     next()
@@ -25,6 +29,37 @@ export function runServer(prisma: PrismaClient, port: number) {
 
   app.use(error.notFoundHandler)
   app.use(error.handler)
+
+  const resolvers = {
+    Query: {
+      allContentItems: () => {
+        return prisma.contentItem.findMany()
+      },
+    },
+  }
+
+  const typeDefs = `
+  type contentItem {
+    uid: String!
+    title: String
+  }
+
+  type Query {
+    allContentItems: [contentItem!]!
+  }
+`
+  const schema = makeExecutableSchema({
+    resolvers,
+    typeDefs,
+  })
+  Routes.use(
+    '/graphql',
+    graphqlHTTP({
+      schema: schema,
+      graphiql: true,
+      context: { prisma: prisma },
+    }),
+  )
 
   return app.listen(port, () => {
     console.log(`Repco server listening on http://localhost:${port}`)
