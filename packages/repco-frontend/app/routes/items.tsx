c4498af5e3db790ab47d2bf8402f6e29e79dc72b
nemi1012 <nemi1012@h-ka.de>
2022-10-02 23:04:06 +0200

    infinitescroll works need to refactor

--- packages/repco-frontend/app/routes/items.tsx
+++ packages/repco-frontend/app/routes/items.tsx
@@ -1,16 +1,20 @@
+/*
+ * Infinite Scroll using Remix Run
+ * Based on client-side Scroll position
+ * Full Article here: https://dev.to/ptenteromano/infinite-scroll-with-remix-run-1g7
+ */
 import stylesUrl from '~/styles/index.css'
-import type { LinksFunction, LoaderFunction } from '@remix-run/node'
-import { json } from '@remix-run/node'
+import type { LinksFunction } from '@remix-run/node'
+import { json, LoaderFunction } from '@remix-run/node'
 import { useFetcher, useLoaderData } from '@remix-run/react'
-import { useCallback, useEffect, useRef, useState } from 'react'
-import { useVirtual } from 'react-virtual'
+import { useCallback, useEffect, useState } from 'react'
 import { gql } from 'urql'
+import { SanitizedHTML } from '~/components/sanitized-html'
 import { graphqlQuery } from '~/lib/graphql.server'
 
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: stylesUrl }]
 }
-
 const QUERY = gql`
   query LoadContentItems(
     $first: Int
@@ -35,141 +39,124 @@ const QUERY = gql`
   }
 `
 
-const LIMIT = 10
-
-const getEndCourser = (searchParams: URLSearchParams) => ({
-  after: searchParams.get('after'),
+const getPage = (searchParams: URLSearchParams) => ({
+  after: searchParams.get('page'),
 })
 
-// type LoaderData = {
-//   items: Array<{ id: string; value: string }>
-//   totalItems: number
-// }
-
 export const loader: LoaderFunction = async ({ request }) => {
-  const courser = getEndCourser(new URL(request.url).searchParams)
-  // console.log('CURSOR', courser)
-
+  const courser = getPage(new URL(request.url).searchParams)
+  console.log('COURSER', courser.after)
   const data = await graphqlQuery(QUERY, {
     first: 10,
     last: null,
     after: courser.after,
     before: null,
   })
-
-  // totalItems: await countItems(),
-
+  //console.log(data)
   return json(data)
 }
 
-export default function Index() {
+export default function Photos() {
   const { data } = useLoaderData()
-  //console.log('DATA ITEMS', data)
-  const [contentItems, setContentItems] = useState(data.contentItems)
-  const [items, setItems] = useState(data.contentItems.nodes)
-
+  const [pageInfo, setPageInfo] = useState(data.contentItems.pageInfo)
+  const [photos, setPhotos] = useState(data.contentItems.nodes)
   const fetcher = useFetcher()
 
-  const page = useRef(contentItems.pageInfo.endCursor)
-  const parentRef = useRef<HTMLDivElement>(null)
+  const [scrollPosition, setScrollPosition] = useState(0)
+  const [clientHeight, setClientHeight] = useState(0)
+  const [height, setHeight] = useState(null)
 
-  const canFetchMore = items.length < contentItems.totalCount
-  //console.log(canFetchMore)
-  const rowVirtualizer = useVirtual({
-    size: contentItems.totalCount,
-    parentRef,
-    initialRect: { width: 0, height: 800 },
-    estimateSize: useCallback(() => 35, []),
-  })
+  const [shouldFetch, setShouldFetch] = useState(true)
+  const [page, setPage] = useState('')
 
+  // Set the height of the parent container whenever photos are loaded
+  const divHeight = useCallback(
+    (node: any) => {
+      if (node !== null) {
+        setHeight(node.getBoundingClientRect().height)
+      }
+    },
+    [photos.length],
+  )
+
+  // Add Listeners to scroll and client resize
   useEffect(() => {
-    const [lastItem] = [...rowVirtualizer.virtualItems].reverse()
-    // console.log('!')
+    const scrollListener = () => {
+      setClientHeight(window.innerHeight)
+      setScrollPosition(window.scrollY)
+    }
 
-    if (!lastItem) {
+    // Avoid running during SSR
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', scrollListener)
+    }
+
+    // Clean up
+    return () => {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('scroll', scrollListener)
+      }
+    }
+  }, [])
+
+  // Listen on scrolls. Fire on some self-described breakpoint
+  useEffect(() => {
+    if (!shouldFetch || !height) return
+    if (clientHeight + scrollPosition < height) return
+    console.log('Start Fetching', pageInfo.endCursor)
+    fetcher.load(`/items?page=${pageInfo.endCursor}`)
+    setShouldFetch(false)
+  }, [clientHeight, scrollPosition, fetcher])
+
+  // Merge photos, increment page, and allow fetching again
+  useEffect(() => {
+    // Discontinue API calls if the last page has been reached
+    if (fetcher.data && fetcher.data.length === 0) {
+      setShouldFetch(false)
       return
     }
-    //  console.log('1')
 
-    if (
-      //lastItem.index > items.length - 1 &&
-      canFetchMore &&
-      fetcher.state === 'idle'
-    ) {
-      //  console.log('2')
-
-      fetcher.load(`/items/?after=${page.current}`)
-    }
-  }, [canFetchMore, fetcher, items.length, page, rowVirtualizer.virtualItems])
-
-  useEffect(() => {
+    // Photos contain data, merge them and allow the possiblity of another fetch
     if (fetcher.data) {
-      // console.log('tetch')
-      setItems((prevItems: any) => [
-        ...prevItems,
+      console.log(fetcher.data)
+      setPhotos((prevPhotos: any) => [
+        ...prevPhotos,
         ...fetcher.data.data.contentItems.nodes,
       ])
-      setContentItems(fetcher.data.data.contentItems)
+      setPageInfo(fetcher.data.data.contentItems.pageInfo)
+      setPage(pageInfo.endCursor)
+      if (pageInfo.hasNextPage) {
+        setShouldFetch(true)
+      }
     }
   }, [fetcher.data])
-
+  let x = 0
   return (
-    <main>
-      <div
-        ref={parentRef}
-        className="List"
-        style={{
-          height: `800px`,
-          width: `100%`,
-          overflow: 'auto',
-        }}
-      >
-        <div
-          style={{
-            height: `${rowVirtualizer.totalSize}px`,
-            width: '100%',
-            position: 'relative',
-          }}
-        >
-          {rowVirtualizer.virtualItems.map((virtualRow) => {
-            const { index } = virtualRow
-            const isLoaderRow = index > items.length - 1
-            const item = items[index]
-            console.log(item)
-            //console.log(item)
-            return (
-              <div
-                key={virtualRow.key}
-                className={`list-item ${
-                  virtualRow.index % 2 ? 'list-item--odd' : 'list-item--even'
-                }`}
-                style={{
-                  position: 'absolute',
-                  top: 0,
-                  left: 0,
-                  width: '100% fit-content',
-                  height: `${virtualRow.size}px`,
-                  transform: `translateY(${virtualRow.start}px)`,
-                }}
-              >
-                {isLoaderRow ? (
-                  canFetchMore ? (
-                    'Loading more...'
-                  ) : (
-                    'Nothing more to load'
-                  )
-                ) : (
-                  <>
-                    <span>{index}</span>
-                    <span>{item.uid}</span>
-                    <span>{item.summary}</span>
-                  </>
-                )}
-              </div>
-            )
-          })}
-        </div>
-      </div>
-    </main>
+    <div
+      ref={divHeight}
+      className="container mx-auto space-y-2 md:space-y-0 md:gap-2 md:grid md:grid-cols-2 py-4"
+    >
+      <table className="table">
+        <tr>
+          <th>Nr</th>
+          <th>Title</th>
+          <th>uid</th>
+          <th>zusammenfassung</th>
+        </tr>
+        {photos.map((photo: any, index: any) => {
+          return (
+            <tr key={photo.uid}>
+              <td>{index + 1}</td>
+              <td>{photo.title}</td>
+              <td>{photo.uid}</td>
+              <td>
+                {' '}
+                <SanitizedHTML allowedTags={['a', 'p']} html={photo.summary} />
+              </td>
+            </tr>
+          )
+        })}
+      </table>
+    </div>
   )
 }
