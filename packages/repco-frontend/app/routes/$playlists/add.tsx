7fc68a18e99f1f26066477f8fd1011de209cae46
nemi-notrace <nemi1012@h-ka.de>
2022-10-16 23:27:28 +0200

    playing around

--- packages/repco-frontend/app/routes/$playlists/add.tsx
+++ packages/repco-frontend/app/routes/$playlists/add.tsx
@@ -11,12 +11,9 @@ export const loader: LoaderFunction = async ({ request }) => {
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData()
   const data = formData.get('add-item')
-  const add = formData.get('add-item')
-
-  console.log('DATA', data)
-  console.log('ADD', data)
-
-  return data
+  const add = formData.get('add-to-playlist')
+  if (!add) return data
+  return add
 }
 
 export default function Playlists() {
@@ -35,16 +32,19 @@ export default function Playlists() {
         playlists.map((e: any) => (
           <ul>
             <li className="font-medium">
-              {e}
               <Form method="post">
-                <button
-                  className="mx-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full w-5 h- text-sm dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  value={[e, data]}
-                  name="add-to-playlist"
-                  type="submit"
-                >
-                  +
-                </button>
+                <div className="relative">
+                  {e}
+                  <button
+                    className="text-white mx-6 my-2
+                     bottom-1.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    value={[e, data]}
+                    name="add-to-playlist"
+                    type="submit"
+                  >
+                    +
+                  </button>
+                </div>
               </Form>
             </li>
           </ul>
@@ -54,6 +54,20 @@ export default function Playlists() {
           <h1 className="font-medium leading-tight text-2xl mt-0 mb-2 text-red-600">
             Create at least one Playlist
           </h1>
+          <Form method="post" action="/playlists/new">
+            <div className="flex flex-row py-2 px-4">
+              <input
+                type="text"
+                name="create-playlist"
+                id="create-playlist"
+                className=" block p-2 pl-6 w-1/4 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="enter name..."
+              />
+              <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                create new playlist
+              </button>
+            </div>
+          </Form>
         </div>
       )}
     </div>
