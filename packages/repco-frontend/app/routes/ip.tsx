10b7b66144512c9e4b1d14aa9a35fa7047cc7c8f
nemi-notrace <nemi1012@h-ka.de>
2022-10-20 16:19:44 +0200

    playing around

--- packages/repco-frontend/app/routes/ip.tsx
+++ packages/repco-frontend/app/routes/ip.tsx
@@ -1,17 +1,20 @@
-import { json, LoaderFunction } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import { LoaderFunction } from '@remix-run/node'
+import { useFetcher, useLoaderData } from '@remix-run/react'
+import { useEffect, useRef, useState } from 'react'
 import { gql } from 'urql'
 import { graphqlQuery } from '~/lib/graphql.server'
 
+const FIRST = 50
+
 const QUERY = gql`
   query LoadContentItemsByOffset(
-    $offset: Int
+    $after: Cursor
     $first: Int
     $orderBy: [ContentItemsOrderBy!]
     $includes: String
   ) {
     contentItems(
-      offset: $offset
+      after: $after
       first: $first
       orderBy: $orderBy
       filter: { title: { includes: $includes } }
@@ -31,6 +34,7 @@ const QUERY = gql`
     }
   }
 `
+//TODO intersection observer query via offset .... bevor minus... next + offset
 
 interface parseNumberParams {
   value: string | null
@@ -41,28 +45,70 @@ const parseNumber = ({ value, defaultValue }: parseNumberParams) => {
 }
 
 export const loader: LoaderFunction = async ({ request }) => {
-  console.log('PAGINATED')
   const url = new URL(request.url)
-  const offset = (parseInt(url.searchParams.get('page') || '1') - 1) * 10
-  const take = parseInt(url.searchParams.get('take') || '2')
+  const after = url.searchParams.get('page')
 
   const orderBy = url.searchParams.get('order') || 'TITLE_ASC'
   const includes = url.searchParams.get('includes') || ''
 
-  const { data } = await graphqlQuery(QUERY, {
-    offset: offset,
-    first: take,
+  return await graphqlQuery(QUERY, {
+    after: after,
+    first: FIRST,
     orderBy: orderBy,
     includes: includes,
-  })
-  return json({
-    nodes: data.contentItems.nodes,
-    count: data.contentItems.totalCount,
   })
 }
 
 export default function Ip() {
-  const { nodes, count } = useLoaderData<typeof loader>()
+  const { data } = useLoaderData<typeof loader>()
+  const [pageInfo, setPageInfo] = useState(data.contentItems.pageInfo)
+  const [itemsToRender, setItemsToRender] = useState(data.contentItems.nodes)
+  const [fetchMore, setFetchMore] = useState(true)
 
-  return nodes.map((e) => <div>{e.uid}</div>)
+  const fetcher = useFetcher()
+  const parentRef = useRef<any>()
+  const startRef = useRef<any>()
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries: any) => {
+      console.log(entries)
+      const entry = entries[0]
+      const start = entries[1]
+      if (entry.isIntersecting) {
+        if (!pageInfo.hasNextPage) return
+        if (pageInfo.hasNextPage) {
+          fetcher.load(`/ip?page=${pageInfo.endCursor}`)
+          if (fetcher.data) {
+            setItemsToRender(fetcher.data.data.contentItems.nodes)
+          }
+          setPageInfo(fetcher.data.data.contentItems.pageInfo)
+        }
+      }
+
+      if (start.isIntersecting) {
+        if (pageInfo.hasPreviousPage) return
+        if (pageInfo.hasPreviousPage) {
+          fetcher.load(`/ip?page=${pageInfo.startCursor}`)
+          if (fetcher.data) {
+            setItemsToRender(fetcher.data.data.contentItems.nodes)
+          }
+          setPageInfo(fetcher.data.data.contentItems.pageInfo)
+        }
+      }
+    })
+
+    observer.observe(parentRef.current)
+    observer.observe(startRef.current)
+  })
+
+  return (
+    <div>
+      <div className="start" ref={startRef}></div>
+      {itemsToRender.map((e: any, index: any) => (
+        <div className="h-10" key={e.uid}>
+          {index}: {e.uid}
+        </div>
+      ))}
+      <div className="end" ref={parentRef}></div>
+    </div>
+  )
 }
