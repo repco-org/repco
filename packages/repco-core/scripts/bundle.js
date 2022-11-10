5ab4ff5d71267d32cce8c030b10bd8ac2826c624
Franz Heinzmann (Frando) <frando@unbiskant.org>
2022-11-10 02:17:27 +0100

    chore: fix linter

--- packages/repco-core/scripts/bundle.js
+++ packages/repco-core/scripts/bundle.js
@@ -1,24 +1,28 @@
 import fs from 'fs'
 import esbuild from 'esbuild'
 
+const entryPoints = ['dist/src/cli/bin.js']
 const outfile = 'dist/cli-bundle.js'
+const metafile = 'dist/cli-bundle.meta.json'
 const watch = !!process.argv.find((x) => x === '-w')
+
 const onRebuild = (error, result) => {
-  if (error) console.error('bundle build failed: ', error)
-  else console.log('bundle build complete. watching for changes.')
-  if (result.warnings.length) console.log('Warnings: \n' + result.warnings.map(x => '* ' + x).join('\n'))
-  if (result.errors.length) console.log('Errors: \n' + result.errors.map(x => '* ' + x).join('\n'))
+  if (error) return console.error('bundle build failed: ', error)
+  console.log('bundle build complete. watching for changes...')
+  if (result.warnings.length) console.log('Warnings:', result.warnings)
+  if (result.errors.length) console.log('Errors:', result.errors)
 }
+
 esbuild
   .build({
-    entryPoints: ['dist/src/cli/bin.js'],
+    entryPoints,
     bundle: true,
     format: 'esm',
     watch: watch && { onRebuild },
     platform: 'node',
     external: ['@prisma/client', 'classic-level'],
     banner: {
-      js: "import {createRequire} from 'module';const require=createRequire(import.meta.url);",
+      js: 'import {createRequire} from "module";const require=createRequire(import.meta.url);',
     },
     metafile: !watch,
     outfile,
@@ -27,10 +31,7 @@ esbuild
     if (watch) console.log('watching...')
     else console.log('wrote ' + outfile)
     if (result.metafile) {
-      fs.writeFileSync(
-        'dist/cli-bundle.meta.json',
-        JSON.stringify(result.metafile),
-      )
+      fs.writeFileSync(metafile, JSON.stringify(result.metafile))
     }
   })
   .catch((err) => console.error(err) || process.exit(1))
