8581d2906fcc499d417bd71eeac025cf6a9df6ed
Franz Heinzmann (Frando) <frando@unbiskant.org>
2022-11-10 01:02:21 +0100

    chore: try to fix test setup for CI

--- packages/repco-core/test/util/setup.ts
+++ packages/repco-core/test/util/setup.ts
@@ -1,21 +1,21 @@
 import 'source-map-support/register.js'
+import getPort from 'get-port'
 import { Test } from 'brittle'
 import {
   ChildProcess,
   spawn as spawnProcess,
   SpawnOptions,
 } from 'node:child_process'
-import { PrismaClient } from '../../lib.js'
 import type { Prisma } from 'repco-prisma'
+import { PrismaClient } from '../../lib.js'
 
 const COMPOSE_FILE = '../../test/docker-compose.test.yml'
-const RUNNING_DBS = new Set()
 
 type SetupOpts = {
   port?: number
 }
 export async function setup(test: Test, opts: SetupOpts = {}) {
-  const pgPort = opts.port || 20432
+  const pgPort = opts.port || (await getPort())
   const databaseUrl = await setupDb(test, pgPort)
   process.env.DATABASE_URL = databaseUrl
   let log: Prisma.LogDefinition[] = []
@@ -27,30 +27,53 @@ export async function setup(test: Test, opts: SetupOpts = {}) {
     },
   })
   // @ts-ignore
-  prisma.$on("query", async (e: any) => {
+  prisma.$on('query', async (e: any) => {
     if (process.env.QUERY_LOG) {
-      console.log(`${e.query} ${e.params}`)
+      test.comment(`QUERY: ${e.query} ${e.params}`)
     }
   })
   return prisma
 }
 
 export async function setup2(test: Test) {
-  return Promise.all([
-    setup(test, { port: 15000 }),
-    setup(test, { port: 15001 }),
-  ])
+  const first = await setup(test)
+  const second = await setup(test)
+  return [first, second]
 }
 
 export async function setupDb(test: Test, port: number) {
   if (process.env.DOCKER_SETUP === '0') return
   const env = {
+    ...process.env,
     POSTGRES_PORT: '' + port,
     DATABASE_URL: `postgresql://test:test@localhost:${port}/tests`,
   }
-  if (!RUNNING_DBS.has(port)) {
-    RUNNING_DBS.add(port)
-    await spawn(
+  const verbose = !!process.env.VERBOSE
+  await spawn(
+    'docker',
+    [
+      'compose',
+      '-p',
+      'repco-postgres-test-' + port,
+      '-f',
+      COMPOSE_FILE,
+      'up',
+      '-d',
+      '--remove-orphans',
+    ],
+    {
+      verbose,
+      log: test.comment,
+      env,
+    },
+  )
+  await spawn('yarn', ['prisma', 'migrate', 'reset', '-f', '--skip-generate'], {
+    log: test.comment,
+    verbose,
+    env,
+  })
+  test.teardown(() => {
+    spawn(
       'docker',
       [
         'compose',
@@ -58,53 +81,35 @@ export async function setupDb(test: Test, port: number) {
         'repco-postgres-test-' + port,
         '-f',
         COMPOSE_FILE,
-        'up',
-        '-d',
-        '--remove-orphans',
+        'down',
       ],
-      {
-        log: test.comment,
-        env,
-      },
+      { verbose },
+    ).catch((err) =>
+      test.comment('Failed to teardown docker container: ' + err.message),
     )
-  }
-  await spawn('yarn', ['prisma', 'migrate', 'reset', '-f', '--skip-generate'], {
-    cwd: '../repco-prisma',
-    log: test.comment,
-    // stdio: 'inherit',
-    env,
-  })
+  }, {})
   return env.DATABASE_URL
 }
-
-process.on('beforeExit', () => {
-  if (process.env.DOCKER_SETUP === '0') return
-  for (const port of RUNNING_DBS) {
-    RUNNING_DBS.delete(port)
-    spawn('docker', [
-      'compose',
-      '-p',
-      'repco-postgres-test-' + port,
-      '-f',
-      COMPOSE_FILE,
-      'down'
-    ]).catch((err) => console.error('>> Failed to teardown docker container:', err))
-  }
-})
 
 function spawn(
   command: string,
   args: string[],
-  opts: SpawnOptions & { log?: Test['comment'] } = {},
+  opts: SpawnOptions & { log?: Test['comment']; verbose?: boolean } = {},
 ): Promise<void> & { child: ChildProcess } {
   if (!opts.stdio) opts.stdio = 'pipe'
-  if (!opts.log) opts.log = (msg: string) => console.error(`# ${msg}`)
-  opts.log(`spawn: ${command} ${args.map((x) => `${x}`).join(' ')}`)
+  const log = opts.log || ((msg: string) => console.error(`# ${msg}`))
+  log(`spawn: ${command} ${args.map((x) => `${x}`).join(' ')}`)
   const child = spawnProcess(command, args, opts)
   let stderr = ''
   let stdout = ''
-  child.stderr?.on('data', (data) => (stderr += data))
-  child.stdout?.on('data', (data) => (stdout += data))
+  child.stderr?.on('data', (data) => {
+    if (opts.verbose) log(data.toString())
+    else stderr += data
+  })
+  child.stdout?.on('data', (data) => {
+    if (opts.verbose) log(data.toString())
+    else stdout += data
+  })
   const promise = new Promise((resolve, reject) => {
     child.on('error', (err) => reject(err))
     child.on('exit', (code) => {
@@ -112,7 +117,9 @@ function spawn(
         const log = stdout + '\n' + stderr
         reject(
           new Error(
-            `Command \`${command}\` exited with code ${code}. Command output:\n${log}`,
+            `Command \`${command}\` exited with code ${code}. ${
+              opts.verbose ? '' : `Command output:\n${log}`
+            }`,
           ),
         )
       } else {
