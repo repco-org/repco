180aaf51db4e1d7b8fbafb7bc332d4c9dfdfafe8
nemi-notrace <nemi1012@h-ka.de>
2022-11-14 23:34:14 +0100

    playing around

--- packages/repco-frontend/app/components/ui/Dropdown.tsx
+++ packages/repco-frontend/app/components/ui/Dropdown.tsx
@@ -1,41 +1,90 @@
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import React from 'react'
 import { HamburgerMenuIcon } from '@radix-ui/react-icons'
-import { Button, NavButton } from './Button'
+import { NavLink } from '@remix-run/react'
+import { cva, cx, VariantProps } from 'class-variance-authority'
+import { Button } from './Button'
 
-const DropdownMenuDemo = (props: ButtonProps) => {
-  const [bookmarksChecked, setBookmarksChecked] = React.useState(true)
-  const [urlsChecked, setUrlsChecked] = React.useState(false)
-  const [person, setPerson] = React.useState('pedro')
+const stylesDropdownMenu = cva('', {
+  variants: {
+    disabled: {
+      true: 'opacity-70 pointer-events-none cursor-not-allowed',
+    },
+    size: {
+      md: 'py-1 px-3 text-base ',
+      sm: 'py-1 px-2 text-sm',
+    },
+    variant: {
+      default: [
+        'radix-side-top:animate-slide-up radix-side-bottom:animate-slide-down',
+        'w-48 rounded-lg px-1.5 py-1 shadow-md md:w-56',
+        'bg-white',
+      ],
+    },
+    item: {
+      default: [
+        'flex-col cursor-default select-none items-center rounded-md px-2 py-2 text-sm outline-none',
+        'text-gray-800 focus:bg-gray-50 dark:focus:bg-gray-900',
+      ],
+
+      bare: '',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+    item: 'default',
+    size: 'sm',
+  },
+})
+
+export interface DropdownMenuBaseProps
+  extends VariantProps<typeof stylesDropdownMenu> {}
+
+export type DropdownMenuProps = DropdownMenuBaseProps
+
+export function DropdownMenuCards(props: DropdownMenuProps) {
+  const className = cx(stylesDropdownMenu(props))
 
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
-        <Button variantSize="icon">
+        <Button variantSize={'icon'}>
           <HamburgerMenuIcon />
         </Button>
       </DropdownMenu.Trigger>
 
       <DropdownMenu.Portal>
-        <DropdownMenu.Content
-          className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-          sideOffset={5}
-        >
-          <DropdownMenu.Item className="DropdownMenuItem">
-            <NavButton variant="bare" to="#">
-              Test
-            </NavButton>
+        <DropdownMenu.Content className={className} align="end" sideOffset={5}>
+          <DropdownMenu.Item className="block py-2 px-4 hover:bg-gray-100">
+            <NavLink to="#">add to Playlist</NavLink>
           </DropdownMenu.Item>
-          <DropdownMenu.Item className="DropdownMenuItem">
-            New Window <div className="RightSlot">⌘+N</div>
+
+          <DropdownMenu.Item
+            disabled={true}
+            className="block py-2 px-4 text-gray-400 hover:bg-gray-100 pointer-events-none cursor-not-allowed"
+          >
+            <NavLink to="#">Transcription</NavLink>
           </DropdownMenu.Item>
-          <DropdownMenu.Item className="DropdownMenuItem" disabled>
-            New Private Window <div className="RightSlot">⇧+⌘+N</div>
+          <DropdownMenu.Item
+            disabled={true}
+            className="block py-2 px-4 text-gray-400 hover:bg-gray-100 pointer-events-none cursor-not-allowed"
+          >
+            <NavLink to="#">Translation</NavLink>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item
+            disabled={true}
+            className="block py-2 px-4 text-gray-400 hover:bg-gray-100 pointer-events-none cursor-not-allowed"
+          >
+            <NavLink to="#">get similar items</NavLink>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item
+            disabled={true}
+            className="block py-2 px-4 text-gray-400 hover:bg-gray-100 pointer-events-none cursor-not-allowed"
+          >
+            <NavLink to="#">recommend/share</NavLink>
           </DropdownMenu.Item>
         </DropdownMenu.Content>
       </DropdownMenu.Portal>
     </DropdownMenu.Root>
   )
 }
-
-export default DropdownMenuDemo
