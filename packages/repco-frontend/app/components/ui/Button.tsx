4844633658304571e27d3b79e947d72ba95ff65e
nemi-notrace <nemi1012@h-ka.de>
2022-11-14 15:26:30 +0100

    playing around

--- packages/repco-frontend/app/components/ui/Button.tsx
+++ packages/repco-frontend/app/components/ui/Button.tsx
@@ -1,53 +1,50 @@
 import { NavLink, NavLinkProps } from '@remix-run/react'
 import type { VariantProps } from 'class-variance-authority'
 import { cva, cx } from 'class-variance-authority'
+import { ClassProp } from 'class-variance-authority/dist/types'
 
-const styles = cva(
-  ' border rounded-md items-center transition-colors duration-100 cursor-default disabled:opacity-50',
-  {
-    variants: {
-      disabled: {
-        true: 'opacity-70 pointer-events-none cursor-not-allowed',
-      },
-      variantSize: {
-        icon: '!p-1',
-        md: 'py-1 px-3 text-md font-medium',
-        sm: 'py-1 px-2 text-sm font-medium',
-      },
-      variant: {
-        default: [
-          'text-blue-700',
-          'bg-white-700 hover:bg-blue-200 placeholder:focus:ring-blue-300',
-          'dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800',
-          'focus:ring-4 focus:outline-none inline-flex',
-        ],
-        inline: [
-          'text-white',
-          'bg-blue-700 hover:bg-blue-800 placeholder:focus:ring-blue-300',
-          'dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800',
-          'absolute right-5 bottom-0.5 focus:ring-4 focus:outline-none',
-          'py-1 px-2 text-sm font-medium',
-        ],
-
-        bare: '',
-      },
+const styles = cva('', {
+  variants: {
+    disabled: {
+      true: 'opacity-70 pointer-events-none cursor-not-allowed',
     },
-    defaultVariants: {
-      variantSize: 'sm',
-      variant: 'default',
+    variantSize: {
+      icon: '!p-1',
+      md: 'py-1 px-3 text-base ',
+      sm: 'py-1 px-2 text-sm',
+    },
+    variant: {
+      default: [
+        'border rounded-md items-center transition-colors duration-100 cursor-default disabled:opacity-50',
+        'text-blue-700',
+        'bg-white-700 hover:text-purple-500 placeholder:focus:ring-purple-500',
+        'focus:ring-2 focus:outline-none inline-flex',
+      ],
+
+      bare: '',
     },
   },
-)
+  defaultVariants: {
+    variantSize: 'sm',
+    variant: 'default',
+  },
+})
+
+export interface ButtonBaseProps extends VariantProps<typeof styles> {}
+
+export type ButtonProps = ButtonBaseProps &
+  React.ButtonHTMLAttributes<HTMLButtonElement> &
+  ClassProp
+
+export type NavButtonProps = ButtonBaseProps &
+  React.ButtonHTMLAttributes<HTMLButtonElement> &
+  NavLinkProps &
+  ClassProp
 
 export function Button(props: ButtonProps) {
   const className = cx(styles(props))
   return <button className={className} {...props} />
 }
-
-export type ButtonProps = ButtonBaseProps &
-  React.ButtonHTMLAttributes<HTMLButtonElement>
-
-export interface ButtonBaseProps extends VariantProps<typeof styles> {}
 
 export function NavButton(props: NavButtonProps) {
   const className = cx(styles(props))
@@ -102,7 +99,3 @@ export function PrevButton(props: NavButtonProps) {
     </NavButton>
   )
 }
-
-export type NavButtonProps = ButtonBaseProps &
-  React.ButtonHTMLAttributes<HTMLButtonElement> &
-  NavLinkProps
--- packages/repco-frontend/app/components/ui/logo.tsx
+++ packages/repco-frontend/app/components/ui/logo.tsx
@@ -1,7 +1,7 @@
 export function Logo() {
   return (
-    <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-      <span className="text-transparent bg-clip-text bg-gradient-to-r to-blue-400 from-blue-800">
+    <h1 className=" text-5xl font-extrabold ">
+      <span className="text-transparent bg-clip-text bg-gradient-to-r to-purple-500 from-blue-700">
         REPCO
       </span>
     </h1>
