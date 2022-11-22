7910f303f6c83b4954f2d6c8bcb86b68b6e406d4
nemi-notrace <nemi1012@h-ka.de>
2022-11-22 14:59:20 +0100

    fix

--- packages/repco-frontend/app/routes/playlists.tsx
+++ packages/repco-frontend/app/routes/playlists.tsx
@@ -1,12 +1,12 @@
+import { Outlet } from '@remix-run/react'
 import { NewPlaylistBar } from '~/components/ui/bars/NewPlaylistBar'
-import PlaylistIndex from './$playlists'
 
 export default function Playlists() {
   return (
     <div>
       <NewPlaylistBar />
 
-      <PlaylistIndex />
+      <Outlet />
     </div>
   )
 }
