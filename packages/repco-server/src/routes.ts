b69d130a5c814b1072c574e8e1240e5ffb0f393d
Franz Heinzmann (Frando) <frando@unbiskant.org>
2023-02-02 13:59:10 +0100

    server: mount frontend conditionally
    also work around weirdnesses in remix and an incompatibility between
    web-stream-polyfill (which remix attaches to globalThis) and
    node:stream/web (which undici's fetch uses)
    
    this is not so nice, but works for now.

--- packages/repco-server/src/routes.ts
+++ packages/repco-server/src/routes.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 import apiRoutes from './routes/api.js'
-import frontendRoutes from './routes/frontend.js'
+import { enableFrontend, router as frontendRoutes } from './routes/frontend.js'
+
+// Enable frontend. This could be put behind a environement variable.
+enableFrontend()
 
 const router = express.Router()
 router.use('/api', apiRoutes)
--- packages/repco-server/src/routes/frontend.ts
+++ packages/repco-server/src/routes/frontend.ts
@@ -1,30 +1,81 @@
-import remix from '@remix-run/express'
 import express from 'express'
 import p from 'path'
-// @ts-ignore
-import build from 'repco-frontend/build/index.js'
 import { createRequire } from 'module'
+import { log } from 'repco-common'
 
-const FRONTEND_BASE_PATH = frontendBasePath()
+const handlers: Record<string, express.Handler> = {}
 
-const router = express.Router()
-
-router.use(
-  '/build',
-  express.static(frontendPath('public/build'), {
-    immutable: true,
-    maxAge: '1y',
-  }),
-)
-router.use(express.static(frontendPath('public'), { maxAge: '1h' }))
-router.all('*', remix.createRequestHandler({ build }))
-
-export default router
-
-function frontendPath(path: string) {
-  return p.join(FRONTEND_BASE_PATH, path)
+const notFoundHandler: express.Handler = (_req, res) => {
+  res.status(404)
+  res.end('Not found')
 }
+
+export function enableFrontend() {
+  mountRemixFrontend().catch((err) =>
+    log.warn('Frontend is not available: ' + err),
+  )
+}
+
+async function mountRemixFrontend() {
+  try {
+    const frontendPath = frontendBasePath()
+    const { createRequestHandler } = await import(
+      '@remix-run/express/dist/server.js'
+    )
+    // @ts-ignore
+    const { default: build } = await import('repco-frontend/build/index.js')
+    await installGlobals()
+
+    handlers.build = express.static(p.join(frontendPath, 'public/build'), {
+      immutable: true,
+      maxAge: '1y',
+    })
+    handlers.static = express.static(p.join(frontendPath, 'public'), {
+      maxAge: '1h',
+    })
+    handlers.main = createRequestHandler({ build })
+  } catch (err) {
+    log.warn('Frontend is not available: ' + err)
+  }
+}
+
+export const router = express.Router()
+
+router.use('/build', (...args) =>
+  handlers.build ? handlers.build(...args) : notFoundHandler(...args),
+)
+router.use((...args) =>
+  handlers.static ? handlers.static(...args) : notFoundHandler(...args),
+)
+router.all('*', (...args) => {
+  handlers.main ? handlers.main(...args) : notFoundHandler(...args)
+})
+
 function frontendBasePath() {
   const require = createRequire(import.meta.url)
   return p.dirname(require.resolve('repco-frontend/package.json'))
+}
+
+export async function installGlobals() {
+  const { fetch, FormData, Headers, Request, Response } = await import(
+    '@remix-run/web-fetch'
+  )
+  const { Blob, File } = await import('@remix-run/web-file')
+  const { ReadableStream, WritableStream } = await import('stream/web')
+  globalThis.Blob = Blob
+  globalThis.File = File
+  // @ts-ignore
+  globalThis.Headers = Headers
+  // @ts-ignore
+  globalThis.Request = Request
+  // @ts-ignore
+  globalThis.Response = Response
+  // @ts-ignore
+  globalThis.fetch = fetch
+  // @ts-ignore
+  globalThis.FormData = FormData
+  // @ts-ignore
+  globalThis.ReadableStream = ReadableStream
+  // @ts-ignore
+  globalThis.WritableStream = WritableStream
 }
