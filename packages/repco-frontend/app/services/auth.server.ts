e6b3fa6360ad83d764675bcf523ff10b0b068817
Franz Heinzmann (Frando) <frando@unbiskant.org>
2023-02-07 14:30:22 +0100

    frontend: fix callback URL

--- packages/repco-frontend/app/services/auth.server.ts
+++ packages/repco-frontend/app/services/auth.server.ts
@@ -2,11 +2,13 @@ import { Authenticator } from 'remix-auth'
 import { GitHubStrategy, SocialsProvider } from 'remix-auth-socials'
 import { sessionStorage } from './session.server'
 
-const BASE_URL = process.env.BASE_URL || window.location.origin
-const CALLBACK_URL =
-  BASE_URL + BASE_URL.endsWith('/')
-    ? ''
-    : '/' + SocialsProvider.GITHUB + '/callback'
+function callbackURL() {
+  let base = process.env.BASE_URL
+  if (!base && typeof window !== 'undefined') base = window.location.origin
+  if (!base) base = 'http://localhost:8765'
+  if (!base.endsWith('/')) base += '/'
+  return base + SocialsProvider.GITHUB + '/callback'
+}
 
 export const authenticator = new Authenticator(sessionStorage, {
   sessionKey: '_session',
@@ -31,7 +33,7 @@ authenticator.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID || '',
       clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
-      callbackURL: CALLBACK_URL
+      callbackURL: callbackURL()
     },
     handleSocialAuthCallback,
   ),
