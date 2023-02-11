727e44c03c244f883cb5ce8c03ffd599f18e3e80
Franz Heinzmann (Frando) <frando@unbiskant.org>
2023-02-11 23:19:09 +0100

    fix(docs-website): add file omitted from previous commit

--- packages/repco-docs-website/app/routes/$.tsx
+++ packages/repco-docs-website/app/routes/$.tsx
@@ -1,12 +1,12 @@
 import type { LoaderFunction } from '@remix-run/node' // or cloudflare/deno
 import { useLoaderData } from '@remix-run/react'
 import { Page } from '~/components/page'
-import { loadSlug } from '~/lib/util.server'
+import { loadPage } from '~/lib/util.server'
 
 export const loader: LoaderFunction = async ({ params }) => {
   const slug = params['*']
   if (!slug) throw new Response('Not found', { status: 404 })
-  const page = await loadSlug(slug)
+  const page = await loadPage(slug)
   return page
 }
 
