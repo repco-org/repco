f67e199e44a187fae60b8e385cbf0c3a630978af
twallner <thomas.wallner@waretec.at>
2024-02-20 09:41:20 +0100

    host fixed

--- packages/repco-graphql/src/plugins/content-item-filter.ts
+++ packages/repco-graphql/src/plugins/content-item-filter.ts
@@ -24,7 +24,7 @@ const ContentItemFilterPlugin = makeAddPgTableConditionPlugin(
       _source: false,
     }
 
-    var url = 'http://es01/_search'
+    var url = 'http://es01:9200/_search'
     const response = fetch(url, {
       method: 'POST',
       headers: {
