0e901de790a1618d3558966c4211a6a4f5b6a63b
twallner <thomas.wallner@waretec.at>
2024-04-09 10:03:55 +0200

    line ending fix

--- packages/repco-graphql/src/lib.ts
+++ packages/repco-graphql/src/lib.ts
@@ -73,7 +73,7 @@ export function getPostGraphileOptions() {
       connectionFilterRelations: true,
     },
     watchPg: true,
-    disableQueryLog: false, //process.env.NODE_ENV !== 'development',
+    disableQueryLog: process.env.NODE_ENV !== 'development',
     // pgDefaultRole:
     //   process.env.NODE_ENV === 'development' ? 'graphql' : 'viewer',
   }
